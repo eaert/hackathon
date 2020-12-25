46a3b61cc836c80ea9641b48d66ff5eb63aad0a4
Eran German <eaertg@gmail.com>
2020-12-25 17:11:48 +0200

    Connection between Server and Client by UDP Protocol

--- client.py
+++ client.py
@@ -3,8 +3,20 @@ import socket
 HOST = '172.1.0.115'
 PORT = 13117
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
-print('Received', repr(data))
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.connect((HOST, PORT))
+#     s.sendall(b'Hello, world')
+#     data = s.recv(1024)
+# print('Received', repr(data))
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+
+# Enable broadcasting mode
+client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+client.bind((HOST, PORT))
+while True:
+    data, addr = client.recvfrom(1024)
+    print("received message: %s"%data)
--- server.py
+++ server.py
@@ -1,16 +1,35 @@
 import socket
+import time
 
 HOST = '172.1.0.115'
 PORT = 13117
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.bind((HOST, PORT))
+#     s.listen()
+#     print('Server started, listening on IP address {}'.format(HOST))
+#     conn, addr = s.accept()
+#     with conn:
+#         print('Connected by', addr)
+#         while True:
+#             data = conn.recv(1024)
+#             if not data:
+#                 break
+#             conn.sendall(data)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+
+# Enable broadcasting mode
+server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+# Set a timeout so the socket does not block
+# indefinitely when trying to receive data.
+server.settimeout(0.2)
+
+message = b"Come Fight Me ! I'll beat you, then will go to buy new keyboard."
+while True:
+    server.sendto(message, (HOST, PORT))
+    print('Server started, listening on IP address {}'.format(HOST))
+    time.sleep(1)
