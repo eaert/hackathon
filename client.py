44756eaf1f5b80d018d78a07af3b0751d73161e8
Eran German <eaertg@gmail.com>
2020-12-25 16:44:36 +0200

    Successfully connection between server and client

--- client.py
+++ client.py
@@ -1,2 +1,10 @@
-for i in range(10):
-    print(i+1)
+import socket
+
+HOST = '172.1.0.115'
+PORT = 13117
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+print('Received', repr(data))
