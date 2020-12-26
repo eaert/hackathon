522bbaa0f5c885a1a177372abf29c41aa62947cc
Eran German <eaertg@gmail.com>
2020-12-26 18:08:56 +0200

    Changes

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
+import scapy.all as scap
 
-HOST = '172.1.0.115'
+HOST = '0.0.0.0'
 PORT = 13117
 
 # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -20,3 +21,20 @@ client.bind((HOST, PORT))
 while True:
     data, addr = client.recvfrom(1024)
     print("received message: %s"%data)
+    ip =scap.IP(src='', dst=addr)
+    SYN = scap.TCP(sport='2115', dst='')
+
+class GameClient:
+
+    def GameClient(self):
+        self.gameClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+        self.gameClient.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+
+        # Enable broadcasting mode
+        self.gameClient.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    def findserver(self):
+        while True:
+            data, addr = client.recvfrom(1024)
+            print("received message: %s"%data)
--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 import socket
 import time
+import scapy.all as scap
+import struct
 
 HOST = '172.1.0.115'
 PORT = 13117
+
+scap.sniff
 
 # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 #     s.bind((HOST, PORT))
@@ -33,3 +37,20 @@ while True:
     server.sendto(message, (HOST, PORT))
     print('Server started, listening on IP address {}'.format(HOST))
     time.sleep(1)
+
+class GameServer:
+
+    def GameServer(self):
+        self.gameServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+        self.gameServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+
+        # Enable broadcasting mode
+        self.gameServer.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    def broadcast(self, message, host, port):
+        print('Server started, listening on IP address {}'.format(HOST))
+        while True:
+            self.gameServer.sendto(message, (HOST, PORT))
+            time.sleep(1)
+    
