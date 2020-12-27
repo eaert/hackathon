a11a80ad86f56d62c8f67c1845a54a23f3d9d92b
Eran German <eaertg@gmail.com>
2020-12-27 16:29:22 +0200

    Connecting with TCP Protocol

--- client.py
+++ client.py
@@ -1,25 +1,25 @@
 import socket
 
-HOST = '0.0.0.0'
+HOST = '172.1.0.115'
 PORT = 13117
 
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     s.connect((HOST, PORT))
-#     s.sendall(b'Hello, world')
-#     data = s.recv(1024)
-# print('Received', repr(data))
+# # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+# #     s.connect((HOST, PORT))
+# #     s.sendall(b'Hello, world')
+# #     data = s.recv(1024)
+# # print('Received', repr(data))
 
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+# client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
-client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+# client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 
-# Enable broadcasting mode
-client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+# # Enable broadcasting mode
+# client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-client.bind((HOST, PORT))
-while True:
-    data, addr = client.recvfrom(1024)
-    print("received message: %s"%data)
+# client.bind((HOST, PORT))
+# while True:
+#     data, addr = client.recvfrom(1024)
+#     print("received message: %s"%data)
     
 
 class GameClient:
@@ -41,23 +41,30 @@ class GameClient:
 
         self.gameClientTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
 
+        self.LookingForGame()
+
     def LookingForGame(self):
         self.gameClientTCP.bind((self.IP, self.Port))
+        print("Client started, listening for offer requests...")
         while True:
-            data, addr = client.recvfrom(20)
-            serverPort = data[:15]
+            data, addr = self.gameClientUDP.recvfrom(20)
+            message = data.decode()
+            serverPort = message[13:]
 
             #checking message
-            if data is None:
+            if message[:10] != '0xfeedbeef':
                 continue
 
+            print("Received offer from %s, attempting to connect...", addr)
             # Data is good, moving to next Level
             self.ConnectingToGame(addr, serverPort)
             
 
     def ConnectingToGame(self, addr, gamePort):
-        connection = self.gameClientTCP.connect((addr, serverPort))
+        connection = self.gameClientTCP.connect((addr, gamePort))
+        self.PlayGame(connection)
+
+    def PlayGame(self, opponent):
         pass
 
-    def PlayGame(self):
-        pass
+GameClient(HOST, PORT)
--- server.py
+++ server.py
@@ -3,41 +3,44 @@ import time
 import struct
 
 HOST = '172.1.0.115'
-PORT = 13117
+PORT = 2115
 
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     s.bind((HOST, PORT))
-#     s.listen()
+# # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+# #     s.bind((HOST, PORT))
+# #     s.listen()
+# #     print('Server started, listening on IP address {}'.format(HOST))
+# #     conn, addr = s.accept()
+# #     with conn:
+# #         print('Connected by', addr)
+# #         while True:
+# #             data = conn.recv(1024)
+# #             if not data:
+# #                 break
+# #             conn.sendall(data)
+
+# server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+# server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+
+# # Enable broadcasting mode
+# server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+# # Set a timeout so the socket does not block
+# # indefinitely when trying to receive data.
+# server.settimeout(0.2)
+
+# message = b"Come Fight Me ! I'll beat you, then will go to buy new keyboard."
+# while True:
+#     server.sendto(message, (HOST, PORT))
 #     print('Server started, listening on IP address {}'.format(HOST))
-#     conn, addr = s.accept()
-#     with conn:
-#         print('Connected by', addr)
-#         while True:
-#             data = conn.recv(1024)
-#             if not data:
-#                 break
-#             conn.sendall(data)
-
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-
-# Enable broadcasting mode
-server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-
-# Set a timeout so the socket does not block
-# indefinitely when trying to receive data.
-server.settimeout(0.2)
-
-message = b"Come Fight Me ! I'll beat you, then will go to buy new keyboard."
-while True:
-    server.sendto(message, (HOST, PORT))
-    print('Server started, listening on IP address {}'.format(HOST))
-    time.sleep(1)
+#     time.sleep(1)
 
 class GameServer:
 
-    def __init__(self):
+    def __init__(self, IP, PORT):
+        self.IP = IP
+        self.Port = PORT
+
         self.gameServerUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
         self.gameServerUDP.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
@@ -45,7 +48,7 @@ class GameServer:
         # Enable broadcasting mode
         self.gameServerUDP.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-        self.broadcast(1,2,3)
+        self.broadcast(self.IP, self.Port)
 
         self.gameServerTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
         self.gameServerTCP.bind((HOST, PORT))
@@ -53,15 +56,17 @@ class GameServer:
 
         self.TCP_Connection()
 
-    def broadcast(self, message, host, port):
+    def broadcast(self, host, port):
         print('Server started, listening on IP address {}'.format(HOST))
         while True:
-            self.gameServerUDP.sendto(message, (HOST, PORT))
+            message = '0xfeedbeef0x2'+str(self.Port)
+            self.gameServerUDP.sendto(bytes(message, 'utf8'), (HOST, 13117))
             time.sleep(1)
 
 
     def TCP_Connection(self):
         self.gameServerTCP.accept()
 
+GameServer(HOST, PORT)
         
     
